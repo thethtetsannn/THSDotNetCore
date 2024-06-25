f8398df33ee408f95b244dd56e33ee1b09b8898e
Thet Htet San <thethtetsan246@gmail.com>
2024-06-25 10:09:02 +0630

    [Completed]

--- THSDotNetCore.ConsoleApp/Program.cs
+++ THSDotNetCore.ConsoleApp/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System.Data;
 using System.Data.SqlClient;
-using System.Data.SqlTypes;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello Thet Htet San");
 
 SqlConnectionStringBuilder stringbuilder = new SqlConnectionStringBuilder();
 stringbuilder.DataSource = "DESKTOP-J7SLJLC";
@@ -12,22 +11,23 @@ stringbuilder.Password = "sa@123";
 
 SqlConnection connection = new SqlConnection(stringbuilder.ConnectionString);
 connection.Open();
-Console.WriteLine("Connection Oepn");
+Console.WriteLine("Connection Open");
 
 string query = "select * from tbl_blog";
-SqlCommand cmd = new SqlCommand(query, connection); 
-SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+SqlCommand cmd = new SqlCommand(query,connection);
+SqlDataAdapter ad = new SqlDataAdapter(cmd);   
 DataTable dt = new DataTable();
-sqlDataAdapter.Fill(dt);
+ad.Fill(dt);
+
 connection.Close();
 Console.WriteLine("Connection Close");
 
-foreach (DataRow dr in dt.Rows)
+foreach(DataRow dr in dt.Rows)
 {
     Console.WriteLine("Blog Id => " + dr["BlogId"]);
     Console.WriteLine("Blog Title => " + dr["BlogTitle"]);
     Console.WriteLine("Blog Author => " + dr["BlogAuthor"]);
     Console.WriteLine("Blog Content => " + dr["BlogContent"]);
-    Console.WriteLine("The End ");
+    Console.WriteLine("----------");
 }
-Console.ReadKey(); 
+Console.ReadKey();
